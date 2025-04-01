747853adf36bc4012e191b1a69e188b999e8a30c
Roelof Ruis <roelof.ruis@gmail.com>
2025-04-02 01:34:55 +0200

    durations

--- durations.hs
+++ durations.hs
@@ -2,6 +2,6 @@
 
 for file in $(find "pub" -type f -name '*.mp3')
 do
-    echo "$file"
-    ffmpeg -i "$file" 2>&1 | grep Duration;
+    echo -e "\033[0;33m${file}\033[0m"
+    ffmpeg -i "$file" 2>&1 | grep Duration | cut -c 13-23
 done
